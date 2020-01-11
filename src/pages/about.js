fb94b44655c0e184e0de9b9dd3fbf107c31d93de
unique.mo <myx3901051@qq.com>
2020-01-11 23:45:30 +0800

    feat: add about page, archives page file

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO title='About page' keywords={['FrontEnd Developer', 'React', 'Node', 'Gatsby']} />
+      <div>to do</div>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/archives.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+
+const ArchivesPage = () => {
+  return (
+    <Layout>
+      <SEO title='Archives page' />
+      <div>to do</div>
+    </Layout>
+  )
+}
+
+export default ArchivesPage
