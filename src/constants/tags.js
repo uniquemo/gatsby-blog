ffde6afe896885a9eb99cb20469005d9ed5f16a9
unique.mo <myx3901051@qq.com>
2020-03-20 14:19:12 +0800

    feat(blog): add 0014_宏观视角下的浏览器--Chrome架构

--- src/constants/tags.js
+++ src/constants/tags.js
@@ -2,14 +2,15 @@ const TAGS_COLORS = {
   typescript: 'is-primary',
   javascript: 'is-warning',
   webpack: 'is-link',
-  network: 'is-info',
-  browser: 'is-success',
+  网络: 'is-info',
+  浏览器: 'is-success',
   node: 'is-danger',
   react: 'is-light',
   摘抄: 'is-primary',
   英语: 'is-warning',
   面试: 'is-info',
-  todo: 'is-danger'
+  todo: 'is-danger',
+  算法与数据结构: 'is-success'
 }
 
 export default TAGS_COLORS
