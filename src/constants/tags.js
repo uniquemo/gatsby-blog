0084e59597e65e8332711766ee03cc77b28a5de4
unique.mo <myx3901051@qq.com>
2020-04-06 00:18:53 +0800

    feat(blog): add 写作那些事 时间精力管理&写作话题方法

--- src/constants/tags.js
+++ src/constants/tags.js
@@ -4,13 +4,14 @@ const TAGS_COLORS = {
   webpack: 'is-link',
   网络: 'is-info',
   浏览器: 'is-success',
-  node: 'is-danger',
+  'Node.js': 'is-danger',
   react: 'is-light',
   摘抄: 'is-primary',
   英语: 'is-warning',
   面试: 'is-info',
   todo: 'is-danger',
-  算法与数据结构: 'is-success'
+  算法与数据结构: 'is-success',
+  阅读写作: 'is-primary'
 }
 
 export default TAGS_COLORS
