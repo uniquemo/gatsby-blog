6bc3b70d8a33c58c8ea53b24cffc0c165d3644e6
unique.mo <myx3901051@qq.com>
2020-05-19 17:29:03 +0800

    feat(blog): add 0037_从头开始搭建React-TypeScript项目

--- src/constants/tags.js
+++ src/constants/tags.js
@@ -5,7 +5,6 @@ const TAGS_COLORS = {
   网络: 'is-info',
   浏览器: 'is-success',
   'Node.js': 'is-danger',
-  react: 'is-light',
   摘抄: 'is-primary',
   英语: 'is-warning',
   面试: 'is-info',
@@ -13,7 +12,8 @@ const TAGS_COLORS = {
   算法与数据结构: 'is-success',
   阅读写作: 'is-primary',
   编辑器: 'is-link',
-  工程化: 'is-danger'
+  工程化: 'is-danger',
+  react: 'is-warning'
 }
 
 export default TAGS_COLORS
